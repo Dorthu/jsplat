a498ca15e6ac2f2ed1b25f6fa54650db26e29572
Dorthu <dorthu2@gmail.com>
2017-08-30 20:11:44 -0400

    I found this when vacating my old work macbook

--- src/main.js
+++ src/main.js
@@ -37,10 +37,10 @@ geo.vertices.push(new THREE.Vector3(300,0,0));
 const line = new THREE.Line(geo, linemat);
 scene.add(line);
 
-const thingmat = new THREE.SpriteMaterial({color: 0x992233 });
-const tgeo = new THREE.PlaneGeometry(20,20);
-const mesh = new THREE.Sprite(thingmat);
-mesh.scale.set(40,40);
+const thingmat = new THREE.MeshBasicMaterial({color: 0x992233 });
+const tgeo = new THREE.CircleGeometry(20);
+const mesh = new THREE.Mesh(tgeo, thingmat);
+mesh.rotation.y = 2 * Math.PI / 2;
 mesh.position.set(0,0,0);
 scene.add(mesh);
 
@@ -51,10 +51,31 @@ ground.createFixture(planck.Edge(planck.Vec2(-300,0), planck.Vec2(300,0)), {
 });
 
 const thing = world.createDynamicBody(planck.Vec2(0.0, -250.0));
-thing.createFixture(planck.Polygon([ planck.Vec2(20.0,20.0), planck.Vec2(-20.0, 20.0),
-        planck.Vec2(-20.0, -20.0), planck.Vec2(20.0, -20.0) ]));
+const circle = thing.createFixture(planck.Circle(),{ density: 1.0 });
+console.log(circle);
+circle.m_shape.m_radius = 20;
+
+const spin_part  = world.createJoint(planck.WheelJoint({
+    motorSpeed : 0.0,
+    maxMotorTorque : 20.0,
+    enableMotor : true,
+    frequencyHz : 4.0,
+    dampingRatio : 0.7
+  }, thing, circle, thing.getPosition(), planck.Vec2(0.0, 0.0)));
+
+
+const sgeo = new THREE.PlaneGeometry(10, 10);
+const squareMesh = new THREE.Mesh(sgeo, thingmat);
+squareMesh.rotation.y = 2 * Math.PI / 2;
+squareMesh.position.set(0,0,0);
+scene.add(squareMesh);
+
+const box = world.createDynamicBody(planck.Vec2(-150,-10));
+const boxFix = box.createFixture(planck.Box(5, 5), { density: .5 });
 
 console.log(thing);
+
+let force=planck.Vec2(0,0);
 
 /// end testing stuff
 
@@ -64,12 +85,19 @@ let lastTime = startTime;
 function render() {
     /// DO TICK
     const new_time = new Date().getTime();
-    if(!paused)
+    if(!paused) {
+        thing.applyForce(force, thing.getWorldCenter());
         world.step(new_time-lastTime);
+        console.log(thing);
+    }
 
     if(thing && mesh) {
         const cpos = thing.getPosition();
         mesh.position.set(cpos.x, cpos.y, 0);
+    }
+    if(box && squareMesh) {
+        const cpos = box.getPosition();
+        squareMesh.position.set(cpos.x, cpos.y, 0);
     }
 
     lastTime = new_time;
@@ -93,4 +121,12 @@ document.addEventListener('keydown', (e) => {
         document.step();
     else if(e.keyCode == 80)
         paused = !paused;
+    else if(e.keyCode == 37) { /// <-
+        force = planck.Vec2(-20,0);
+    } else if(e.keyCode == 39) { /// ->
+        force = planck.Vec2(20,0);
+    } else if(e.keyCode == 38) { /// up
+        console.log('this');
+        force = planck.Vec2(0, -20);
+    }
 });
